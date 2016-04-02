9618cf0fb967d5fcbd79550d9c42444d7452d0a8
telegrambot81 <telegrambot81@gmail.com>
2016-04-02 14:07:29 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -246,7 +246,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {120629055,106789762},
     admin_users = {},
     disabled_channels = {}
   }
